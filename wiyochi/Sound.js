275b9f656d4352fcc3fd3a12638f16552e0188ff
Mathieu ARQUILLIERE <Mathieu ARQUILLIERE>
2017-04-08 21:10:32 +0200

    Sound.js->Music.js + Sound.js

--- wiyochi/Sound.js
+++ wiyochi/Sound.js
@@ -1,59 +1,29 @@
-class Music{
-	constructor(path, nbMusic){
-		this.nbMusic = nbMusic;
-		this.arrayPath = new Array();
+class Sound{
+	constructor(){
+		this.nbSound = 1;
+		this.path = "resources/sounds/";
+		this.pathArray = new Array();
 
-		for (var i=0; i<nbMusic; i++){
-			this.arrayPath.push(path + "song" + String(i) + ".mp3");
+		for (var i=0; i<this.nbSound; i++){
+			this.pathArray.push(path + "lvl_" + String(i) + ".mp3");
 		}
 
-		this.arrayMusic = new Array();
-		this.random = -1;
+		this.soundArray = new Array();
 	}
 
 	load(){
-		color(255, 0, 0);
-		text("TEST", 50, 50);
-		/*
-		for(var i=0; i<(this.nbMusic); i++){
-			this.arrayMusic.push(loadSound(this.arrayPath[i]));
+		for(var i=0; i<this.nbSound; i++){
+			this.soundArray.push(loadSound(this.pathArray[i]));
 		}
-		*/
 	}
 
 	volume(newVolume){
-		for(var i=0; i<(this.nbMusic); i++){
-			this.arrayMusic[i].setVolume(newVolume);
+		for(var i=0; i<this.nbSound; i++){
+			this.soundArray[i].setVolume(newVolume);
 		}
 	}
 
-	playMusic(){
-		alert("test");
-		var rand;
-		do {
-			rand = Math.random() * this.arrayMusic.length();
-		} while(rand==this.random);
-		this.random = rand;
-		this.arrayMusic[this.random].play();
-	}
-
-	play(){
-		if (!isPlaying()){
-			playMusic();
-		}
+	play(nlvl){
+		this.soundArray[nlvl].play();
 	}
 }
-
-
-
-
-/*
-function preload() {
-  mySound = loadSound('assets/doorbell.mp3');
-}
-
-function setup() {
-  mySound.setVolume(0.1);
-  mySound.play();
-}
-*/
