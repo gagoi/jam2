3d4b73a239e6ec16497fd9d2200c5ad0b8f2de9a
FixyNeko <felix@dhumes.fr>
2017-04-08 18:04:35 +0200

    non

--- FixyNeko/Chapter/Chapter.js
+++ FixyNeko/Chapter/Chapter.js
@@ -4,8 +4,10 @@ var xEnd, yEnd;
 
 function loadLevel(){
     var rawFile = new XMLHttpRequest();
-	rawFile.open("GET", "./test.txt", false);
-	print("text:", rawFile, ":text");
+	rawFile.open("GET", "test.txt", false);
+	rawFile.onreadystatechange = function ()
+    print("text ", rawFile.responseText);
+	rawFile.send(null);
 }
 
 function readTextFile(file)
@@ -19,7 +21,7 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                fileDisplayArea.innerText = allText
+                fileDisplayArea.innerText = allText;
             }
         }
     }
