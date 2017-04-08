06e98b4a882904d7f9e81febd9be93c135a58e62
Mathieu ARQUILLIERE <Mathieu ARQUILLIERE>
2017-04-09 00:12:09 +0200

    Amélio vite fait + comment de Timer

--- wiyochi/Timer.js
+++ wiyochi/Timer.js
@@ -10,9 +10,15 @@ class Timer {
     }
 
     start(){
-		this.date = new Date();
-        this.time = this.date.getTime()-this.currentTime;
-		this.run = true;
+		if(!this.run){
+			this.date = new Date();
+			this.time = this.date.getTime()-this.currentTime;
+			this.run = true;
+		}
+		else{
+			this.reset();
+			this.start();
+		}
     }
 
 	stop(){
@@ -21,6 +27,7 @@ class Timer {
 		this.run = false;
 	}
 
+	//Return timer in milliseconds
 	getTime(){
 		this.date = new Date();
 		if (this.run) {
@@ -32,14 +39,17 @@ class Timer {
 		}
 	}
 
+	//Add time in milliseconds
 	add(addTime){
 		this.time -= addTime;
 	}
 
+	//Starting time in milliseconds
 	setTimeMax(timeMax){
 		this.timeMax = timeMax;
 	}
 
+	//Return timer in String format: "+- min : sec : msec"
 	getDraw(){
 		var tmp = this.getTime();
 		var negative = false;
@@ -47,10 +57,11 @@ class Timer {
 			tmp = -tmp;
 			negative = true;
 		}
-		var msec = Math.floor((tmp%1000)/10);
-		var sec = Math.floor((tmp/1000)%60);
-		var min = Math.floor(((tmp/1000)/60)%60);
+		var msec = Math.floor((tmp%1000)/10);		//Transform
+		var sec = Math.floor((tmp/1000)%60);		//into form
+		var min = Math.floor(((tmp/1000)/60)%60);	//min:sec:msec
 
+		//Set to 0x:0x:0x (ex: 04:05:07)
 		if(msec<10){
 			msec = "0"+String(msec);
 		}
