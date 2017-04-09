af79d5be249626454daeca953c6c5dd14b0a651f
FixyNeko <felix@dhumes.fr>
2017-04-09 13:39:27 +0200

    hitboxes

--- Game.js
+++ Game.js
@@ -22,7 +22,7 @@ class Game{
 		this.player.setHeight(this.playerh);
 		this.updateList[this.updateList.length] = this.player;
 
-		this.selectLevel(0,4);
+		this.selectLevel(0,9);
 	}
 
 	update() {
@@ -32,8 +32,8 @@ class Game{
 		this.player.update();
 
 		if(casePos[1] > 0 && isHard(this.chapter.getCaseId(casePos[0], casePos[1] - 1))) //has a (hard) block on top of it
-			if(collideRectRect(casePos[0] * 32, (this.casePos[1] - 1) * 32, 32, 32, this.player.getxPos(), this.player.getyPos(), this.player.getWidth(), this.player.getHeight())){
-				this.player.setyPos(this.casePos[1] * 32);
+			if(collideRectRect(casePos[0] * 32, (casePos[1] - 1) * 32, 32, 32, this.player.getxPos(), this.player.getyPos(), this.player.getWidth(), this.player.getHeight())){
+				this.player.setyPos(casePos[1] * 32);
 				this.player.setyVel(0);
 			}
 		if(casePos[1] < this.chapter.getMapHeight() - 1 && isHard(this.chapter.getCaseId(casePos[0], casePos[1] + 1))) //has a (hard) block on bottom of it
@@ -41,15 +41,13 @@ class Game{
 				this.player.setyPos((casePos[1] + 1) * 32 - this.player.getHeight());
 				this.player.setyVel(0);
 			}
-		if(casePos[0] >= 1 && isHard(this.chapter.getCaseId(casePos[0] - 1, casePos[1]))) //has a (hard) block on left of it
-			if(collideRectRect((casePos[0] - 1) * 32, casePos[1] * 32, 32, 32, this.player.getxPos(), this.player.getyPos(), this.player.getWidth(), this.player.getHeight())){
-				this.player.setxPos(casePos[0] * 32);
-				this.player.setxVel(0);
+		if(casePos[0] > 0 && isHard(this.chapter.getCaseId(casePos[0] - 1, casePos[1]))) //has a (hard) block on left of it
+			if(collideRectRect((casePos[0] + 1) * 32, casePos[1] * 32, 32, 32, this.player.getxPos(), this.player.getyPos(), this.player.getWidth(), this.player.getHeight())){
+				this.player.setxPos((casePos[0] + 1) * 32);
 			}
 		if(casePos[0] < this.chapter.getMapWidth() - 1 && isHard(this.chapter.getCaseId(casePos[0] + 1, casePos[1]))) //has a (hard) block on right of it
 			if(collideRectRect((casePos[0] + 1) * 32, casePos[1] * 32, 32, 32, this.player.getxPos(), this.player.getyPos(), this.player.getWidth(), this.player.getHeight())){
 				this.player.setxPos((casePos[0] + 1) * 32 - this.player.getWidth());
-				this.player.setxVel(0);
 			}
 	}
 
